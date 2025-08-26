14f1352c148f3ac95ee44f2569b9a6db110d99c3
Shamil Irfan <shamilirfan28@gmail.com>
2025-08-26 17:11:07 +0600

    Single Child Scroll View

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -33,105 +34,25 @@ class HomePage extends StatelessWidget {
         title: Text("This is appBar", style: TextStyle(color: Colors.white)),
         backgroundColor: Colors.green,
       ),
-      body: Column(
-        spacing: 10,
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
+      body: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: Column(
+          children: [
+            for (int i = 1; i <= 10; i++)
               Container(
-                width: 100,
+                width: 500,
                 height: 100,
-                child: Image.network(
-                  'https://tse2.mm.bing.net/th/id/OIP.81qvZOftdwN7DjyHpwbl-wHaF7?r=0&cb=thfc1&rs=1&pid=ImgDetMain&o=7&rm=3',
+                color: const Color.fromARGB(255, 252, 190, 2),
+                margin: EdgeInsets.all(10),
+                child: Center(
+                  child: Text(
+                    "$i",
+                    style: TextStyle(fontSize: 25, color: Colors.white),
+                  ),
                 ),
-                color: const Color.fromARGB(255, 76, 102, 251),
               ),
-              Container(
-                width: 100,
-                height: 100,
-                child: Image.network(
-                  'https://tse1.mm.bing.net/th/id/OIP.dpnzax0mQBqKV5zRwAI7qwHaF7?r=0&cb=thfc1&rs=1&pid=ImgDetMain&o=7&rm=3',
-                ),
-                color: Color.fromARGB(255, 143, 156, 228),
-              ),
-              Container(
-                width: 100,
-                height: 100,
-                child: Image.network(
-                  'https://tse1.mm.bing.net/th/id/OIP.DUZSHUsc0P9humPH9PjKUgHaE6?r=0&cb=thfc1&rs=1&pid=ImgDetMain&o=7&rm=3',
-                ),
-                color: Color.fromARGB(255, 180, 189, 239),
-              ),
-            ],
-          ),
-          Column(
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Container(
-                    width: 100,
-                    height: 100,
-                    child: Image.network(
-                      'https://tse1.mm.bing.net/th/id/OIP.DUZSHUsc0P9humPH9PjKUgHaE6?r=0&cb=thfc1&rs=1&pid=ImgDetMain&o=7&rm=3',
-                    ),
-                    color: const Color.fromARGB(255, 255, 212, 58),
-                  ),
-                  Container(
-                    width: 100,
-                    height: 100,
-                    child: Image.network(
-                      'https://tse2.mm.bing.net/th/id/OIP.81qvZOftdwN7DjyHpwbl-wHaF7?r=0&cb=thfc1&rs=1&pid=ImgDetMain&o=7&rm=3',
-                    ),
-                    color: const Color.fromARGB(255, 244, 221, 136),
-                  ),
-                  Container(
-                    width: 100,
-                    height: 100,
-                    child: Image.network(
-                      'https://tse1.mm.bing.net/th/id/OIP.dpnzax0mQBqKV5zRwAI7qwHaF7?r=0&cb=thfc1&rs=1&pid=ImgDetMain&o=7&rm=3',
-                    ),
-                    color: const Color.fromARGB(255, 236, 230, 207),
-                  ),
-                ],
-              ),
-            ],
-          ),
-          Column(
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Container(
-                    width: 100,
-                    height: 100,
-                    child: Image.network(
-                      'https://tse1.mm.bing.net/th/id/OIP.dpnzax0mQBqKV5zRwAI7qwHaF7?r=0&cb=thfc1&rs=1&pid=ImgDetMain&o=7&rm=3',
-                    ),
-                    color: const Color.fromARGB(255, 255, 58, 179),
-                  ),
-                  Container(
-                    width: 100,
-                    height: 100,
-                    child: Image.network(
-                      'https://tse1.mm.bing.net/th/id/OIP.DUZSHUsc0P9humPH9PjKUgHaE6?r=0&cb=thfc1&rs=1&pid=ImgDetMain&o=7&rm=3',
-                    ),
-                    color: const Color.fromARGB(255, 241, 160, 201),
-                  ),
-                  Container(
-                    width: 100,
-                    height: 100,
-                    child: Image.network(
-                      'https://tse2.mm.bing.net/th/id/OIP.81qvZOftdwN7DjyHpwbl-wHaF7?r=0&cb=thfc1&rs=1&pid=ImgDetMain&o=7&rm=3',
-                    ),
-                    color: const Color.fromARGB(255, 245, 206, 228),
-                  ),
-                ],
-              ),
-            ],
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
