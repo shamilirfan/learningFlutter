b30d3045559404dfa1b065bb6f5c6f482871d556
Shamil Irfan <shamilirfan28@gmail.com>
2025-08-26 18:32:39 +0600

    Grid View

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,4 @@
-import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
 
 void main() {
   runApp(const MyApp());
@@ -30,6 +28,13 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var images = <String>[
+      'https://c4.wallpaperflare.com/wallpaper/327/865/306/boat-tropical-beach-island-cove-hd-wallpaper-preview.jpg',
+      'https://tse4.mm.bing.net/th/id/OIP.liYTHdMyJZHuCIs7PyQU2AHaD5?r=0&cb=thfc1&w=1900&h=1000&rs=1&pid=ImgDetMain&o=7&rm=3',
+      'https://tse3.mm.bing.net/th/id/OIP.dvKl7mBOSq01p_H5LpPOBgHaEK?r=0&cb=thfc1&rs=1&pid=ImgDetMain&o=7&rm=3',
+      'https://tse2.mm.bing.net/th/id/OIP.s4SAePe1s_gS9SuCj6HJ9wHaEC?r=0&cb=thfc1&rs=1&pid=ImgDetMain&o=7&rm=3',
+      'https://tse1.mm.bing.net/th/id/OIP.bDX9zbqNPSe1w8AHDycYFwHaEK?r=0&cb=thfc1&w=1024&h=575&rs=1&pid=ImgDetMain&o=7&rm=3',
+    ];
     return Scaffold(
       appBar: AppBar(
         title: Text("This is appBar", style: TextStyle(color: Colors.white)),
@@ -37,21 +42,14 @@ class HomePage extends StatelessWidget {
       ),
       body: GridView(
         gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 3,
+          crossAxisCount: 2,
         ),
         children: [
-          for (int i = 0; i < 20; i++)
+          for (int i = 0; i < 5; i++)
             Container(
-              width: 100,
-              height: 100,
-              margin: EdgeInsets.all(5),
+              margin: EdgeInsets.all(3),
               color: Colors.purple,
-              child: Center(
-                child: Text(
-                  'Hi',
-                  style: TextStyle(fontSize: 25, color: Colors.white),
-                ),
-              ),
+              child: Image.network('${images[i]}'),
             ),
         ],
       ),
