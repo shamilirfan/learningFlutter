13dcaa098b0be924807f8dd26a4186d798b68d02
Shamil Irfan <shamilirfan28@gmail.com>
2025-09-02 09:03:06 +0600

    sum app

--- lib/main.dart
+++ lib/main.dart
@@ -19,33 +19,44 @@ class MyApp extends StatelessWidget {
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
-
   @override
   State<StatefulWidget> createState() => HomePageUI();
 }
 
 class HomePageUI extends State<HomePage> {
-  // Input Decoration
-  InputDecoration inputDecoration(label) {
+  // style text field
+  InputDecoration inputDecoration(labelTxt) {
     return InputDecoration(
-      border: const OutlineInputBorder(),
-      labelText: label,
+      labelText: labelTxt,
+      border: OutlineInputBorder(),
+      filled: true,
+      fillColor: Colors.white,
+      hoverColor: Colors.white,
     );
   }
 
-  // Map for form values
-  Map<String, double> formValue = {"Number1": 0, "Number2": 0};
-  double sum = 0;
-
-  // method to set input value in map safely
-  inputOnChange(inputKey, inputValue) {
-    setState(() {
-      formValue.update(inputKey, (value) => double.parse(inputValue));
-    });
+  // button style
+  ButtonStyle buttonStyle() {
+    return ElevatedButton.styleFrom(
+      backgroundColor: Colors.teal,
+      minimumSize: Size(double.infinity, 55),
+      textStyle: TextStyle(fontWeight: FontWeight.bold),
+    );
   }
 
-  // Sum all numbers
-  sumAllNumber() {
+  // map
+  Map<String, double> formValue = {"form1": 0, "form2": 0};
+  double sum = 0;
+
+  // on change function
+  onChanged(inputKey, inputValue) {
+    setState(
+      () => formValue.update(inputKey, (value) => double.parse(inputValue)),
+    );
+  }
+
+  // sum All
+  sumAll() {
     setState(() {
       for (var item in formValue.values) {
         sum += item;
@@ -56,46 +67,27 @@ class HomePageUI extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Sum App"),
-        backgroundColor: Colors.blueGrey,
-      ),
+      appBar: AppBar(title: Text('Sum App'), backgroundColor: Colors.green),
       body: Padding(
-        padding: const EdgeInsets.fromLTRB(30, 20, 30, 0),
+        padding: EdgeInsets.fromLTRB(30, 20, 30, 0),
         child: Column(
           children: [
-            Text("Sum = $sum", style: const TextStyle(fontSize: 20)),
-            const SizedBox(height: 20),
-
-            // First Number Input
+            Text('Sum = $sum', style: TextStyle(fontSize: 25)),
+            SizedBox(height: 20),
             TextField(
-              keyboardType: TextInputType.number,
-              onChanged: (value) => inputOnChange("Number1", value),
-              decoration: inputDecoration('First Number'),
+              onChanged: (value) => onChanged('form1', value),
+              decoration: inputDecoration("First Number"),
             ),
-            const SizedBox(height: 20),
-
-            // Second Number Input
+            SizedBox(height: 20),
             TextField(
-              keyboardType: TextInputType.number,
-              onChanged: (value) => inputOnChange("Number2", value),
-              decoration: inputDecoration('Second Number'),
+              onChanged: (value) => onChanged('form2', value),
+              decoration: inputDecoration("Second Number"),
             ),
-            const SizedBox(height: 20),
-
-            // Button
+            SizedBox(height: 20),
             ElevatedButton(
-              onPressed: () => sumAllNumber(),
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.purple,
-                shadowColor: Colors.pinkAccent,
-                elevation: 5,
-                minimumSize: const Size(double.infinity, 60),
-              ),
-              child: const Text(
-                "Add",
-                style: TextStyle(fontWeight: FontWeight.bold),
-              ),
+              onPressed: () => sumAll(),
+              style: buttonStyle(),
+              child: Text("Add"),
             ),
           ],
         ),
